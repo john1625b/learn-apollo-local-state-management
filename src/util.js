6175f7d899e90be2073f6085c2fc4f891813812e
John <john1625b@gmail.com>
2021-07-03 04:34:43 -0400

    updates readme

--- src/util.js
+++ src/util.js
@@ -4,10 +4,11 @@ export const convertPrice = async (originalCurrency, newCurrency, amount) => {
   if (originalCurrency === newCurrency)
     return amount
   // fetch from api
-  const { data } = await axios.get(`https://api.exchangeratesapi.io/latest?symbols=${newCurrency}&base=${originalCurrency}`)
+  const API_key = 'b47581b00aab4f45e5fd';
+  const { data } = await axios.get(`https://free.currconv.com/api/v7/convert?q=${originalCurrency}_${newCurrency}&compact=ultra&apiKey=${API_key}`)
 
   // extract the conversion rate multipler
-  const conversionMultiplier = Object.values(data.rates)[0]
+  const conversionMultiplier = Object.values(data)[0]
   //
   return amount * conversionMultiplier
 }
